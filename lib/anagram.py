df40ff86270dac6cb5836e6eb440e23f20673554
leon mwai <leon.mwai@student.moringaschool.com>
2025-05-18 12:48:18 +0300

    added anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        original_sorted = sorted(self.word)
+        matches = []
+
+        for candidate in possible_anagrams:
+            if candidate.lower() != self.word and sorted(candidate.lower()) == original_sorted:
+                matches.append(candidate)
+
+        return matches
